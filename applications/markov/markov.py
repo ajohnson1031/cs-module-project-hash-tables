f6781d96210b895e762d561a2927c0501f58e793
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-07-19 15:45:20 -0400

    reworked markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -14,79 +14,69 @@ with open("input.txt") as f:
 # Your code here
 
 #converts text into an array of words
-s = re.sub(r'[^a-zA-Z\'\.\"]+', ' ', words.strip()).split(" ")
-i = 0
- 
-while i < len(s) - 1:
-    if s[i] in cache: 
-        if s[i+1] not in cache[s[i]]:
-            cache[s[i]].append(s[i+1]) 
-    else:     
-        cache[s[i]] = [s[i+1]] 
-    i+=1
-
-# TODO: construct 5 random sentences
-# Your code here
-
-def make_sentence(max_length=8):
+def split_into_words(txt):
+    s = txt.strip().split(" ")
     i = 0
-    sentence = ""
-   
-    while i < max_length:
-        #get random index
-        rand_cache_index = s[random.randrange(0, len(cache))]
-        #get value at random index (list)
-        rand_word_list = cache[rand_cache_index]
-        #get random word from value at random index
-        rand_word = rand_word_list[random.randrange(0, len(rand_word_list))].strip()
-        
-        """
-        Using random.choice()
-        rand_cache_index = random.choice(s)
-        rand_word_list = cache[rand_cache_index]
-        rand_word = random.choice(rand_word_list)
-        """
-        
-        #case where last char is '.'
-        #adds random word with trailing space to sentence and breaks out of loop, ending sentence
-        if rand_word[-1] == ".":
-            sentence += rand_word + " "
-            break
-        #case where last char is '"' 
-        #since we know the only case where the last char is going to be a quotation mark is at the end of a sentence, we remove the last two chars and append the edited word with a trailing space to the end of the sentence
-        if rand_word[-1] == '"':
-            sentence += rand_word[0:-2] + " "
-            break
-        #case where word is a quotation mark, period or a space, skip
-        #skips to next loop cycle
-        elif rand_word == '"' or rand_word == "." or rand_word == " ":
-            continue
-        #case where both random word and sentence have a quotation mark. 
-        #sanitizes random word, removing quotation, then adds it to the sentence with a trailing space
-        elif '"' in rand_word and '"' in sentence:
-            rand_word = re.sub(r'[^a-zA-Z]+', "", rand_word.strip())
-            sentence += rand_word + " "    
-        #default case
-        #adds random word to sentence with trailing space
-        else:
-            sentence += rand_word + " " 
-          
+    
+    while i < len(s) - 1:
+        if s[i] in cache: 
+            if s[i+1] not in cache[s[i]]:
+                cache[s[i]].append(s[i+1]) 
+        else:     
+            cache[s[i]] = [s[i+1]] 
         i+=1
     
-    #checks number of quotations in sentence and whether or not sentence has a period
-    #if there is a quotation mark and there is no period, it adds a trailing period, followed by a quotation mark and space to the sentence
-    if sentence.count('"') >= 1 and sentence.count('.') == 0:
-        sentence = sentence.strip() + '." '
-    #if there is a quotation mark and a period, just adds the quotation mark and trailing space to the end of sentence  
-    elif sentence.count('"') >= 1 and sentence.count('.') > 0:
-        sentence = sentence.strip() + '" '
-    #if there is no period on the sentence, adds period and trailing space to end
-    elif sentence.count(".") == 0:
-        sentence = sentence.strip() + ". " 
+    return len(s),s,cache
+
+#instantiate split word cache
+s = split_into_words(words)
+
+def get_random_wordlist():
+    return cache[s[1][random.randrange(0, s[0])]]
+
+def get_random_word(wordlist):
+    return random.choice(wordlist).replace('\n', ' ').strip()
+
+def get_random_key():
+    return random.choice(list(cache.keys()))
+
+def get_start_word():
+    start_word = get_random_word(get_random_wordlist())
+    
+    if start_word[0] == '"' or start_word[0].isupper() or start_word[1].isupper():
+        return start_word
+    else:
+        return get_start_word()
+
+def get_end_word():
+    end_word = get_random_word(get_random_wordlist())
+    ew_qualifiers = ['.', '!', '?', '"']
+
+    if end_word[-1] in ew_qualifiers:
+        return end_word
+    else:
+        return get_end_word()
+
+def get_sentence_word():
+    sentence_word = get_random_word(get_random_wordlist())
+    
+    if sentence_word[0].isupper() or sentence_word[0] == '"' or sentence_word[-1] == '"':
+        return get_sentence_word()
+    else:
+        return sentence_word
+
+def make_sentence(limit=8):
+    sentence = get_start_word()
+    end_word = get_end_word()
+    i = 0
+    
+    while i < limit - 2:
+        sentence += " " + get_sentence_word()
+        i+=1
+    
+    sentence += " " + end_word + " "
     
     return sentence
-      
 
 for _ in range(5): paragraph += make_sentence()
-
-print(TGREEN + paragraph, ENDC)
+print(TGREEN, paragraph, ENDC)
